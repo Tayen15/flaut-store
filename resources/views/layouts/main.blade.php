528f391e22bb193a8ccb433cc38d6124d44e47ca
Tayen15 <tiantiantitik@gmail.com>
2024-01-02 16:48:56 +0700

    update laravel error

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,8 +4,13 @@
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>TEST</title>
+     <link href="/dist/output.css" rel="stylesheet">
 </head>
 <body>
-     
+     <header>
+          @include('components.header')
+     </header>
+     <footer>
+     </footer>
 </body>
 </html>
