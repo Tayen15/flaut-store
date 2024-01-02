85073861e394e05c08f0725eb3b45d75834bbd64
pompomv <rifandra0310@gmail.com>
2024-01-02 17:07:39 +0700

    route components dan layouts

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
+Route::get('/components', function () {
+    return view('header');
+});
+
+Route::get('/layouts', function () {
+    return view('home.blade.php');
 });
