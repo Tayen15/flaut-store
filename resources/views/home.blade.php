2113012d92a2cf8aa20ae9756f667e6c5918eeec
citraaudina <cintraaudina4@gmail.com>
2024-01-03 17:09:00 +0700

    footer yang ganti

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 @section('content')
 
-<div class="flex h-screen items-center justify-center bg-no-repeat bg-cover" style="background-image: url('https://cdn.discordapp.com/attachments/976824443743645696/1048464776214356029/IMG_20221203_120411.jpg?ex=65a219ea&is=658fa4ea&hm=e355dfe5b27c67d3218288e3c7f79571159a069f92d89575a727e933ac753474&')">
+<div class="flex h-screen items-center justify-center bg-no-repeat bg-cover" style="background-image: url('https://deagamdesign.com/wp-content/uploads/2019/10/61-Contoh-Desain-Toko-Baju-Minimalis-Di-Rumah-Kreatif-Banget-Deh.jpg')">
      <div class="text-center text-white">
          <h1 class="text-5xl font-bold mb-4">Welcome to Our Website</h1>
          <p class="text-xl mb-8">Discover amazing features and more</p>
