fdf055916a054ddc53002ae06eae6721d6d05eb2
pompomv <rifandra0310@gmail.com>
2024-01-04 14:48:42 +0700

    aboutUs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,25 +25,24 @@
 <div class="sm:flex items-center max-w-screen-xl mt-10 mb-10">
     <div class="sm:w-1/2 p-10">
         <div class="image object-center text-center">
-            <img src="https://cdn.discordapp.com/attachments/976824443743645696/1192114617116266607/WbQnbas.png?ex=65a7e641&is=65957141&hm=0a918bfbf1f86862de9502a7c159061239e6300c28265580b9c589b058545431&">
+            <img src="https://cdn.discordapp.com/attachments/976824443743645696/1192365071083503636/flaut.1_2.png?ex=65a8cf82&is=65965a82&hm=f9183fea4ca358dcf057572f73030e26994e91bf27d7617cf0487ea80d910438&">
         </div>
     </div>
-    <div class="about">
-    <div class="sm:w-1/2 p-5">
+    <div class="about sm:w-1/2 p-5">
         <div class="text">
             <span class="text-gray-500 border-b-2 border-orange-600 uppercase">About us</span>
-            <h2 class="my-4 font-bold text-3xl  sm:text-4xl ">About <span class="text-orange-600">Our Company</span>
-            </h2>
-            <p class="text-gray-700">
-            Dapatkan gaya yang tak tertandingi dengan koleksi terbaru kami! Jelajahi ragam pilihan baju terkini yang dirancang untuk memenuhi segala kebutuhan gaya Anda. Nikmati diskon spesial yang akan membuat pengalaman belanja Anda semakin menggembirakan. 
-            Kami memberikan layanan pengiriman gratis untuk setiap pembelian di atas jumlah tertentu. Belanja sepuasnya tanpa perlu khawatir ongkos kirim!
-             Berkualitas tinggi dan penuh perhatian, setiap produk kami dirancang dengan detail yang cermat untuk memberikan kenyamanan dan gaya tanpa kompromi.
-             Dengan situs web dan aplikasi mobile kami yang user-friendly, belanja baju menjadi lebih mudah dan menyenangkan. Pilih, bayar, dan tunggu paket istimewa Anda tiba di depan pintu.
-            Jangan lewatkan kesempatan ini untuk memperbarui lemari pakaian Anda dengan gaya yang selalu trendi dan terkini. 
-            </p>
+            <h2 class="my-4 font-bold text-3xl  sm:text-4xl ">About <span class="text-orange-600">Our Company</span></h2>
+            <div class="sm:flex">
+                <p class="text-gray-700 sm:w-1/2 pr-2">
+                    Dapatkan gaya yang tak tertandingi dengan koleksi terbaru kami! Jelajahi ragam pilihan baju terkini yang dirancang untuk memenuhi segala kebutuhan gaya Anda. Nikmati diskon spesial yang akan membuat pengalaman belanja Anda semakin menggembirakan. Kami memberikan layanan pengiriman gratis untuk setiap pembelian di atas jumlah tertentu. Belanja sepuasnya tanpa perlu khawatir ongkos kirim!
+                </p>
+                <p class="text-gray-700 sm:w-1/2 pl-2">
+                    Berkualitas tinggi dan penuh perhatian, setiap produk kami dirancang dengan detail yang cermat untuk memberikan kenyamanan dan gaya tanpa kompromi. Dengan situs web dan aplikasi mobile kami yang user-friendly, belanja baju menjadi lebih mudah dan menyenangkan. Pilih, bayar, dan tunggu paket istimewa Anda tiba di depan pintu. Jangan lewatkan kesempatan ini untuk memperbarui lemari pakaian Anda dengan gaya yang selalu trendi dan terkini.
+                </p>
+            </div>
         </div>
     </div>
 </div>
-</div>
+
 
 @endsection
