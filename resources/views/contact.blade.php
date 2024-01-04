05199645f5a28d5f44dd1f5282eaa9da68638d7c
citraaudina <cintraaudina4@gmail.com>
2024-01-05 06:34:03 +0700

    perbaharui contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,6 +1,34 @@
 @extends('layouts.main')
+
 @section('content')
-<div class="flex flex-col h-screen items-center justify-center bg-no-repeat bg-cover">
-     <h1>About US</h1>
+<div class="container my-24 mx-auto md:px-6">
+    <!-- Section: Design Block -->
+    <section class="mb-32 text-center">
+        <div class="mx-auto max-w-[700px] md:px-3 text-black ">
+            <h2 class="mb-12 text-3xl font-bold">Contact us</h2>
+            <form>
+                <div class="relative mb-6 text-black" data-te-input-wrapper-init>
+                    <input type="text" class="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none text-black" id="exampleInputName" />
+                    <label class="pointer-events-none font-bold absolute top-2 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none text-black" for="exampleInputName">Name</label>
+                </div>
+                <div class="relative mb-6 text-black" data-te-input-wrapper-init>
+                    <input type="text" class="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none text-black" id="exampleInputEmail" />
+                    <label class="pointer-events-none font-bold absolute top-2 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none text-black" for="exampleInputEmail">Email address</label>
+                </div>
+                <div class="relative mb-6 text-black" data-te-input-wrapper-init>
+                    <input type="text" class="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none text-black" id="exampleInputMessage" />
+                    <label class="pointer-events-none font-bold absolute top-2 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none text-black" for="exampleInputMessage">Message</label>
+                </div>
+                <div class="mb-6 inline-block min-h-[1.5rem] justify-center pl-[1.5rem] md:flex">
+                <input type="checkbox">
+                        <label>Send me a copy of this message</label>
+                    </div>
+                <button type="submit" class="bg-black text-white px-4 py-2 rounded hover:bg-black focus:outline-none focus:ring focus:border-indigo-500">
+                    Send
+                </button>
+            </form>
+        </div>
+    </section>
+    <!-- Section: Design Block -->
 </div>
 @endsection
