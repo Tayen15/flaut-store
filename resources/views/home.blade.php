5ac037cf2cbf234c935b60bde015adc7e71cdb2f
Tayen15 <tiantiantitik@gmail.com>
2024-01-04 18:39:37 +0700

    add color change for flaut text every 350ms

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,8 @@
 <section id="carousel">
     <div class="flex flex-col h-screen items-center justify-center bg-no-repeat bg-cover" style="background-image: url('https://cdn.discordapp.com/attachments/976824443743645696/1192125787650932856/Flaut.jpg?ex=65a7f0a8&is=65957ba8&hm=c90f5a7586c6bffda6c1734f203e22aa8ffd82118b49aff1f0212d8113aae0fd&'); background-size: cover;">
         <div class="order-2 text-center text-white font-libre-baskerville">
-            <h1 class="text-5xl font-bold mb-4 text-orange-600">F L A U T.</h1>
-            <h2 class="text-5xl font-bold mb-4">Welcome to flaut. fashion store </h2>
+            <h1 id="flaut" class="text-5xl font-bold mb-4 text-orange-600">F L A U T.</h1>
+            <h2 id="" class="text-5xl font-bold mb-4">Welcome to flaut. fashion store </h2>
             <p class="text-xl mb-8">Discover many amazing fashions and more</p>
         </div>
     </div>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -25,34 +25,53 @@
           @include('components.footer')
      </footer>
      <script>
+
+const specificColors = ['rgb(234, 88, 12)', '#FFFFFF', '#000000'];
+
+function getRandomColor() {
+    return specificColors[Math.floor(Math.random() * specificColors.length)];
+}
+
+          function changeColor(element, characters) {
+               const color = getRandomColor();
+               element.style.color = color;
+          }
+
           document.addEventListener("DOMContentLoaded", function () {
-          const navbar = document.getElementById("navbar");
-          const ahref = document.getElementById("text-nav");
-          const ahref2 = document.getElementById("text-nav-2");
-          const ahref3 = document.getElementById("text-nav-3");
-          const logoImage = document.querySelector("#navbar img");
+               const navbar = document.getElementById("navbar");
+               const ahref = document.getElementById("text-nav");
+               const ahref2 = document.getElementById("text-nav-2");
+               const ahref3 = document.getElementById("text-nav-3");
+               const logoImage = document.querySelector("#navbar img");
 
-          function setNavbarColor(scrollPosition) {
-               if (scrollPosition > 0 || window.location.pathname !== "/") {
-                    navbar.classList.remove("bg-transparent");
-                    navbar.classList.add("bg-black");
-                    ahref.classList.add("text-white");
-                    ahref2.classList.add("text-white");
-                    ahref3.classList.add("text-white");
-                    logoImage.classList.add("mt-1");
-               } else {
-                    navbar.classList.remove("bg-black");
-                    navbar.classList.add("bg-transparent");
+               function setNavbarColor(scrollPosition) {
+                    if (scrollPosition > 0 || window.location.pathname !== "/") {
+                         navbar.classList.remove("bg-transparent");
+                         navbar.classList.add("bg-black");
+                         ahref.classList.add("text-white");
+                         ahref2.classList.add("text-white");
+                         ahref3.classList.add("text-white");
+                         logoImage.classList.add("mt-1");
+                    } else {
+                         navbar.classList.remove("bg-black");
+                         navbar.classList.add("bg-transparent");
+                    }
                }
-          }
 
-          function handleScroll() {
+               function handleScroll() {
+                    setNavbarColor(window.scrollY);
+               }
+
                setNavbarColor(window.scrollY);
-          }
 
-          setNavbarColor(window.scrollY);
+               window.addEventListener("scroll", handleScroll);
 
-          window.addEventListener("scroll", handleScroll);
+               const element = document.getElementById('flaut');
+               const charactersToColor = ['F', 'L', 'A', 'U', 'T', '.'];
+
+               setInterval(function () {
+                    changeColor(element, charactersToColor);
+               }, 350);
           });
 
      </script>
