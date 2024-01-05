4c052324a994639f131b63c0a06956ea58078f83
pompomv <rifandra0310@gmail.com>
2024-01-05 19:42:01 +0700

    contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.main')
+@extends('layouts.only-header')
 @section('content')
 <div class="relative flex items-top justify-center min-h-screen bg-white dark:bg-gray-900 sm:items-center sm:pt-0">
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
