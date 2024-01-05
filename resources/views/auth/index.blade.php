f459866829e05271af6889c97b2a5baa2ed1cbe9
Tayen15 <tiantiantitik@gmail.com>
2024-01-05 18:59:54 +0700

    Change label and placeholder from username to email

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -16,8 +16,8 @@
             <form method="POST" action="{{ route('auth') }}">
                 @csrf
                 <div class="mt-3">
-                    <label for="username" class="block text-base mb-2">Username</label>
-                    <input type="text" name="email" class="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600" placeholder="Enter Username..." />
+                    <label for="username" class="block text-base mb-2">Email</label>
+                    <input type="text" name="email" class="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600" placeholder="Enter Email..." />
                 </div>
                 <div class="mt-3">
                     <label for="password" class="block text-base mb-2">Password</label>
