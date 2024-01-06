39872dfe494cf85953dbc5123c7f06ccbcb9d344
Tayen15 <tiantiantitik@gmail.com>
2024-01-06 22:47:47 +0700

    Change background image in landing page to fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 
 {{-- Carousel --}}
 <section id="carousel">
-    <div class="flex flex-col h-screen items-center justify-center bg-no-repeat bg-cover" style="background-image: url('https://cdn.discordapp.com/attachments/976824443743645696/1192125787650932856/Flaut.jpg?ex=65a7f0a8&is=65957ba8&hm=c90f5a7586c6bffda6c1734f203e22aa8ffd82118b49aff1f0212d8113aae0fd&'); background-size: cover;">
+    <div class="flex flex-col h-screen items-center justify-center bg-no-repeat bg-cover bg-fixed" style="background-image: url('https://cdn.discordapp.com/attachments/976824443743645696/1192125787650932856/Flaut.jpg?ex=65a7f0a8&is=65957ba8&hm=c90f5a7586c6bffda6c1734f203e22aa8ffd82118b49aff1f0212d8113aae0fd&'); background-size: cover;">
         <div class="order-2 text-center text-white font-libre-baskerville">
             <h1 id="flaut" class="text-5xl font-bold mb-4 text-orange-600">F L A U T.</h1>
             <h2 id="" class="text-5xl font-bold mb-4">Welcome to flaut. fashion store </h2>
