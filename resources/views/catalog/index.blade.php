d2f6a41f0c684538b23a20679eb7dd733a5b18bd
Tayen15 <tiantiantitik@gmail.com>
2024-01-06 22:15:29 +0700

    Add shadow background for catalog image and the path for image catalog

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -5,16 +5,16 @@
     <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <div class="flex justify-between items-center mb-2">
             <h2 class="text-2xl font-bold tracking-tight text-gray-900">Recomendation Catalog</h2>
-            <a href="{{ route('catalog.create') }}" class="text-white bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded-md text-sm font-medium">Add Catalog</a>
+            <a href="{{ route('catalog.create') }}" class="text-white bg-orange-600 hover:bg-blue-600 px-4 py-2 rounded-md text-sm font-medium">Add Catalog</a>
         </div>
         <a href="{{ route('catalog.index') }}" class="text-sm text-gray-700 hover:text-gray-900">View All</a>
         <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             @foreach($catalogs as $catalog)
             <div class="group relative">
-                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:overflow-hidden group-hover:shadow-xl lg:h-80">
                     <img src="{{ $catalog->image_url }}" alt="{{ $catalog->name }}" class="h-full w-full object-cover object-center lg:h-full lg:w-full">
                 </div>
-                <div class="mt-4 flex justify-between">
+                <div class="mt-4 flex justify-between ">
                     <div>
                         <h3 class="text-sm text-gray-700">
                             <a href="#">
