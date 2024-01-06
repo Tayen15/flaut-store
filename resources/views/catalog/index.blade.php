3d3582d3ea89758593dd6801049dfe1f45945eaa
Tayen15 <tiantiantitik@gmail.com>
2024-01-06 22:04:10 +0700

    Add Button to create

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -3,9 +3,11 @@
 
 <div class="bg-white">
     <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 class="text-2xl font-bold tracking-tight text-gray-900">Recomendation Catalog</h2>
+        <div class="flex justify-between items-center mb-2">
+            <h2 class="text-2xl font-bold tracking-tight text-gray-900">Recomendation Catalog</h2>
+            <a href="{{ route('catalog.create') }}" class="text-white bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded-md text-sm font-medium">Add Catalog</a>
+        </div>
         <a href="{{ route('catalog.index') }}" class="text-sm text-gray-700 hover:text-gray-900">View All</a>
-    
         <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             @foreach($catalogs as $catalog)
             <div class="group relative">
