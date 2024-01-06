abd1c1b811a14a60a229a8ba4ff292bc13c6b29b
Tayen15 <tiantiantitik@gmail.com>
2024-01-06 22:40:31 +0700

    Make read more in image while in hover

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -19,13 +19,15 @@
 
 <section id="news">
     <div class="mx-auto max-w-2xl px-4 sm:px-6 sm:py-14 lg:max-w-7xl ">
-        <div class="flex justify-between items-center mb-2">
-            <a href="{{ route('catalog.create') }}" class="text-white bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded-md text-sm font-medium">Add News</a>
-        </div>
         <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 gap-3 mt-5">
             @forelse($news as $item)
-                <div class="rounded overflow-hidden shadow-lg max-w-xs">
-                    <img class="w-full h-56 object-cover" src="{{ $item->image_url }}" alt="{{ $item->title }}">
+                <div class="group rounded overflow-hidden shadow-lg max-w-xs relative">
+                    <div class="relative">
+                        <img class="w-full h-56 object-cover transition-transform transform group-hover:brightness-75" src="{{ $item->image_url }}" alt="{{ $item->title }}">
+                        <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+                            <a href="#" class="text-white font-light text-lg transition-transform transform hover:rounded-full px-7 py-1 bg-transparent border border-white duration-500 hover:bg-white hover:text-black">Read More</a>
+                        </div>
+                    </div>
                     <div class="px-6 py-2">
                         <div class="font-bold text-xl mb-2">{{ $item->title }}</div>
                         <p class="text-gray-700 text-base">{{ $item->content }}</p>
@@ -35,7 +37,7 @@
                     </div>
                 </div>
             @empty
-                <p class="text-center text-xl font-bold mt-5">No updates available</p>        
+                <p class="text-center text-xl font-bold mt-5">No updates available</p>
             @endforelse
         </div>
     </div>
