2a4d325e824132175cb22ad4de4664339559f8ee
Tayen15 <tiantiantitik@gmail.com>
2024-01-11 15:44:26 +0700

    Change footer newsletter

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,8 @@
 
-<div class="mt-14 mx-auto md:px-6 p-8 bg-fixed w-full mb-5" style="background-image: url('https://cdn.discordapp.com/attachments/976824443743645696/1193148201675526184/download.jpeg?ex=65aba8da&is=659933da&hm=b409f771292a711d2f621606e630ac3a40132c9d011acf468abf86d3129b6c99&')">
+<div class="mt-14 mx-auto md:px-6 p-8 bg-fixed w-full mb-5 bg-cover" style="background-image: url('https://cdn.discordapp.com/attachments/976824443743645696/1194923197624238131/download_1.jpeg?ex=65b21df2&is=659fa8f2&hm=483da8ae0553a06bb2c0d7a035a361f3d33c050798a62eae1e62855bbc826758&')">
      <section class="mb-32 text-center lg:text-left" id="newsletter">
          <div class="flex flex-wrap justify-center mt-3">
-             <div class="w-full shrink-0 grow-0 basis-auto px-3 md:w-10/12 lg:w-11/12 xl:w-10/12">
+             <div class="w-full md:w-10/12 lg:w-11/12 xl:w-10/12">
                  <div class="grid items-center gap-x-6 lg:grid-cols-2">
                      <div class="mb-10 lg:mb-0">
                          <h2 class="text-3xl font-bold dark:text-white">
@@ -11,8 +11,8 @@
                              <span class="text-primary dark:text-primary-400">Subscribe to the newsletter</span>
                          </h2>
                      </div>
-                     <div class="mb-6 flex-row md:mb-0 md:flex">
-                         <div class="relative mb-3 w-full md:mr-3 md:mb-0 xl:w-96" data-te-input-wrapper-init>
+                     <div class="mb-6 md:mb-0 md:flex md:flex-col">
+                         <div class="relative mb-3 w-full md:mr-3 xl:w-96" data-te-input-wrapper-init>
                              <input type="text"
                                  class="peer block min-h-[auto] w-full rounded border-0 bg-white py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-black dark:placeholder:text-black dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                                  id="exampleFormControlInput2" placeholder="Enter your email" />
@@ -22,7 +22,7 @@
                              </label>
                          </div>
                          <button type="submit"
-                             class="inline-block rounded bg-primary px-7 pt-3 pb-2.5 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                             class="inline-block rounded bg-orange-500 px-5 pt-3 pb-2.5 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-orange-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-orange-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-orange-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                              data-te-ripple-init data-te-ripple-color="light">
                              Subscribe
                          </button>
@@ -31,7 +31,8 @@
              </div>
          </div>
      </section>
- </div>
+</div>
+ 
 
 
 <div class="mx-6 py-10 text-center md:text-left px-20">
