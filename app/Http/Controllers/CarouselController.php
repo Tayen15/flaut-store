cbe3a8be8cb1739959261ec0bc490262d9e54056
citraaudina <cintraaudina4@gmail.com>
2024-01-13 22:48:33 +0700

    carousel admin

--- app/Http/Controllers/CarouselController.php
+++ app/Http/Controllers/CarouselController.php
@@ -7,32 +7,17 @@ use Illuminate\Http\Request;
 
 class CarouselController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $query = Carousel::query();
-
-        // Pencarian
-        $searchKeyword = $request->input('search');
-        if ($searchKeyword && strlen($searchKeyword) >= 3) {
-            $query->where('name', 'like', '%' . $searchKeyword . '%');
-        }
-    
-        // Filter Kategori
-        $categories = $request->input('categories', []);
-        if (!empty($categories)) {
-            $query->whereIn('category', $categories);
-        }
-    
-        $Carousels = $query->get();
-        return view('Carousel.index', compact('Carousels'));
+        $Carousel = Carousel::paginate(10);
+        return view('Carousel.index', compact('Carousel'));
     }
+
     public function indexAdmin()
     {
-        $Carousels = Carousel::paginate(10); // Adjust the pagination as needed
-
-        return view('dashboard.Carousel.index', compact('Carousels'));
+        $Carousel = Carousel::paginate(6);
+        return view('dashboard.Carousel.index', compact('Carousel'));
     }
-
 
     public function create()
     {
@@ -41,21 +26,20 @@ class CarouselController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        $validatedData = $request->validate([
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:6144',
             'name' => 'required',
-            
         ]);
-
+    
+        // Ubah direktori penyimpanan gambar
         $imagePath = $request->file('image')->store('image/Carousel');
         $imageName = basename($imagePath);
-
-        $validatedData = $request->except('image');
+    
         $validatedData['image'] = $imageName;
-
+    
         Carousel::create($validatedData);
-
-        return redirect()->route('dashboard.Carousel.index')->with('success', 'Carousel item created successfully');
+    
+        return redirect()->route('dashboard.Carousel.index');
     }
 
     public function show($id)
@@ -73,26 +57,22 @@ class CarouselController extends Controller
     public function update(Request $request, $id)
     {
         $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
-            'description' => 'required|string',
-            'price' => 'required|numeric',
             'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+
+            'name' => 'required',
         ]);
 
-        if ($request->hasFile('image')) {
-            $imagePath = $request->file('image')->store('image/Carousel');
-            $validatedData['image'] = $imagePath;
-        }
-
         Carousel::findOrFail($id)->update($validatedData);
-
-        return redirect()->route('dashboard.Carousel.index')->with('success', 'Carousel item updated successfully');
+        return redirect()
+            ->route('dashboard.Carousel.index')
+            ->with('success', 'Successfully updated Carousel');
     }
 
-    public function destroy($id)
+    public function destroy(Carousel $Carousel)
     {
-        Carousel::findOrFail($id)->delete();
-
-        return redirect()->route('dashboard.Carousel.index')->with('success', 'Carousel item deleted successfully');
+        $Carousel->delete();
+        return redirect()
+            ->route('dashboard.Carousel.index')
+            ->with('success', 'Successfully deleted Carousel');
     }
 }
