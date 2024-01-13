f74c8df7cc50ebb6813a8edb231b64b7e4451266
Tayen15 <tiantiantitik@gmail.com>
2024-01-13 23:37:28 +0700

    Change route to carousel create

--- resources/views/dashboard/carousel/index.blade.php
+++ resources/views/dashboard/carousel/index.blade.php
@@ -8,7 +8,7 @@
             <div class=" p-4 shadow text-2xl text-white">
                 <h1 class="font-bold pl-2">Carousel</h1>
             </div>
-            <a href="{{ route('dashboard.catalog.create') }}" class="flex items-center text-white mr-10">
+            <a href="{{ route('dashboard.carousel.create') }}" class="flex items-center text-white mr-10">
                 <i class="fa-solid fa-plus mr-2"></i> Add Carousel
             </a>
         </div>
