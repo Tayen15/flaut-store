9c7788e821fb333495b7e8d620c11ded17601782
Tayen15 <tiantiantitik@gmail.com>
2024-01-13 18:32:07 +0700

    Add routing to dashboard index in header dashboard admin

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -37,8 +37,8 @@
         <nav aria-label="menu nav" class="bg-gray-800 pt-2 md:pt-1 pb-1 px-1 mt-0 h-auto fixed w-full z-20 top-0">
             <div class="flex flex-wrap items-center">
                 <div class="flex flex-shrink md:w-1/3 justify-center md:justify-start text-white">
-                    <a href="#" aria-label="Home">
-                        <span class="text-xl pl-2"><i class="em em-grinning"></i></span>
+                    <a href="{{ route('dashboard.index') }}" aria-label="Home">
+                        <span class="text-xl pl-2"><i class="em em-grinning"></i>flaut.</span>
                     </a>
                 </div>
                 <div class="flex flex-1 md:w-1/3 justify-center md:justify-start text-white px-2">
