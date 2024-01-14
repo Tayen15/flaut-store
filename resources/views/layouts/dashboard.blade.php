c56dea6b0b9c2817877a768ddc90da063ab71cdd
Tayen15 <tiantiantitik@gmail.com>
2024-01-14 18:04:09 +0700

    add feature search for dashboard admin

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -35,12 +35,14 @@
                 
                 <div class="flex flex-1 md:w-1/3 justify-center md:justify-start text-white px-2 mx-3">
                     <span class="relative w-full">
-                        <input aria-label="search" type="search" id="search" placeholder="Search" class="w-full md:max-w-md lg:max-w-xl bg-gray-900 text-white transition border border-transparent focus:outline-none focus:border-gray-400 rounded py-3 px-2 pl-10 appearance-none leading-normal">
-                        <div class="absolute search-icon" style="top: 1rem; left: .8rem;">
-                            <svg class="fill-current pointer-events-none text-white w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path>
-                            </svg>
-                        </div>
+                        <form action="{{ route('dashboard.index') }}" method="get">
+                            <input aria-label="search" type="text" id="search" name="search" placeholder="Search" class="w-full md:max-w-md lg:max-w-xl bg-gray-900 text-white transition border border-transparent focus:outline-none focus:border-gray-400 rounded py-3 px-2 pl-10 appearance-none leading-normal">
+                            <div class="absolute search-icon" style="top: 1rem; left: .8rem;">
+                                <svg class="fill-current pointer-events-none text-white w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path>
+                                </svg>
+                            </div>
+                        </form>
                     </span>
                 </div>
                 
