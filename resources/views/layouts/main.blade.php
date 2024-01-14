3fe473f4a3314d69d0d3a5b4aade7f7c6d1ea40e
Tayen15 <tiantiantitik@gmail.com>
2024-01-14 14:59:30 +0700

    Removing script js for tiny editor

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,23 +12,8 @@
               
           }
      </style>
+     <script src="https://kit.fontawesome.com/86a839d15c.js" crossorigin="anonymous"></script>
      <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-     <script src="https://cdn.tiny.cloud/1/z72nwuco4n4dl82wa73wr87z45kfsvdnlsiqaj14wwc004gc/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-     <script>
-          tinymce.init({
-            selector: 'textarea',
-            plugins: 'ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
-            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-            tinycomments_mode: 'embedded',
-            tinycomments_author: 'Author name',
-            images_upload_base_path: 'storage/image/news/',
-            mergetags_list: [
-              { value: 'First.Name', title: 'First Name' },
-              { value: 'Email', title: 'Email' },
-            ],
-            ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
-          });
-     </script>
 </head>
 <body class="antialiased">
      <header>
