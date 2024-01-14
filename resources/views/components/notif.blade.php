4f58602c7ea4a5663bc90f59fff558cde275dafc
Tayen15 <tiantiantitik@gmail.com>
2024-01-14 12:44:03 +0700

    Edited UI notif dialog

--- resources/views/components/notif.blade.php
+++ resources/views/components/notif.blade.php
@@ -1,14 +1,14 @@
 <div class="notification-container">
     @if (session('success'))
-    <div role="alert" data-dismissible="alert" class="relative flex w-full max-w-screen-md px-4 py-4 text-base text-white bg-gray-900 rounded-lg font-regular notification">
+    <div role="alert" data-dismissible="alert" class="relative flex w-full max-w-screen-md px-4 py-3 text-base text-white bg-gray-900 rounded-lg font-regular notification">
         <div class="shrink-0">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                 <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd"></path>
             </svg>
         </div>
         <div class="ml-3 mr-12">
-            <h5 class="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-white">
-                Success
+            <h5 class="block font-sans text-lg antialiased font-semibold leading-snug tracking-normal text-white">
+                Information Success
             </h5>
             <p class="block mt-2 font-sans text-base antialiased font-normal leading-relaxed text-white">{{ session('success') }}</p>
         </div>
