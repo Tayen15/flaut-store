53758430ccc115bda7175eff921bcc9d842d7eef
Tayen15 <tiantiantitik@gmail.com>
2024-01-14 22:26:28 +0700

    Tidy up the layout of icons and text so they are aligned

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -84,7 +84,7 @@
                             </li>
                             <li class="mr-3 flex-1">
                                 <a href="{{ route('dashboard.news.index') }}" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-purple-500">
-                                    <i class="fa-regular fa-newspaper pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">News</span>
+                                    <i class="fa-regular fa-newspaper pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs ml-1 md:text-base text-gray-400 md:text-gray-200 block md:inline-block">News</span>
                                 </a>
                             </li>
                             <li class="mr-3 flex-1">
@@ -94,7 +94,7 @@
                             </li>
                             <li class="mr-3 flex-1">
                                 <a href="{{ route('dashboard.carousel.index') }}" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-purple-500">
-                                    <i class="fa-regular fa-image pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Banner</span>
+                                    <i class="fa-regular fa-image pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs ml-1 md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Banner</span>
                                 </a>
                             </li>
                         </ul>
