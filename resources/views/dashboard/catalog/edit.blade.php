8d96fa2253bf31b35776051efb99702f6ece882c
Tayen15 <tiantiantitik@gmail.com>
2024-01-14 22:14:13 +0700

    Auto select options that match existing values ​​in the database

--- resources/views/dashboard/catalog/edit.blade.php
+++ resources/views/dashboard/catalog/edit.blade.php
@@ -26,10 +26,10 @@
                     <div class="relative">
                         <select id="category" name="category" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:outline-none focus:ring appearance-none" required>
                             <option value="" autofocus></option>
-                            <option value="t-shirt">T-Shirt</option>
-                            <option value="shirt">Shirt</option>
-                            <option value="pants">Pants</option>
-                            <option value="accessories">Accessories</option>
+                            <option value="t-shirt" {{ $catalog->category == 't-shirt' ? 'selected' : '' }}>T-Shirt</option>
+                            <option value="shirt" {{ $catalog->category == 'shirt' ? 'selected' : '' }}>Shirt</option>
+                            <option value="pants" {{ $catalog->category == 'pants' ? 'selected' : '' }}>Pants</option>
+                            <option value="accessories" {{ $catalog->category == 'accessories' ? 'selected' : '' }}>Accessories</option>
                         </select>
                         <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                             <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
