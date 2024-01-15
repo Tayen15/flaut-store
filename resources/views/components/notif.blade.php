4e7f8c44d52cf23f669ed2fb84e6da07bba54a39
Tayen15 <tiantiantitik@gmail.com>
2024-01-15 12:50:18 +0700

    Change color svg

--- resources/views/components/notif.blade.php
+++ resources/views/components/notif.blade.php
@@ -24,13 +24,13 @@
     @if (session('error'))
     <div role="alert" data-dismissible="alert" class="relative flex w-full max-w-screen-md px-4 py-3 text-base text-white bg-gray-900 rounded-lg font-regular notification">
         <div class="shrink-0">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-                <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd"></path>
+            <svg fill="currentColor" width="25px" height="25px" viewBox="0 0 256 256" id="Flat" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6">
+                <path d="M128,24A104,104,0,1,0,232,128,104.12041,104.12041,0,0,0,128,24Zm37.65625,130.34375a7.99915,7.99915,0,1,1-11.3125,11.3125L128,139.3125l-26.34375,26.34375a7.99915,7.99915,0,0,1-11.3125-11.3125L116.6875,128,90.34375,101.65625a7.99915,7.99915,0,0,1,11.3125-11.3125L128,116.6875l26.34375-26.34375a7.99915,7.99915,0,0,1,11.3125,11.3125L139.3125,128Z"/>
             </svg>
         </div>
         <div class="ml-3 mr-12">
             <h5 class="block font-sans text-lg antialiased font-semibold leading-snug tracking-normal text-white">
-                Information Success
+                Information Error
             </h5>
             <p class="block mt-2 font-sans text-base antialiased font-normal leading-relaxed text-white">{{ session('success') }}</p>
         </div>
