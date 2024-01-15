ea1e05a65f2e08086737aa607f4fcb4ec8d27cb8
Tayen15 <tiantiantitik@gmail.com>
2024-01-16 01:34:45 +0700

    Removing screen custom

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-    screens: {
-      'phone': '320px',
-    }
-  },
-  plugins: [],
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
 
