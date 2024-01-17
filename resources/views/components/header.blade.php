f72776ac7d631a80d0b4534e97ef57f8f73eab94
Tayen15 <tiantiantitik@gmail.com>
2024-01-17 09:45:19 +0700

    Removing flex mobile navbar

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -41,14 +41,15 @@
     </div>
 </nav>
 
+
 <script>
     document.addEventListener("DOMContentLoaded", function () {
         const mobileMenuBtn = document.getElementById("mobile-menu-btn");
         const mobileMenu = document.getElementById("mobile-menu");
+        mobileMenu.classList.toggle("hidden");
 
         mobileMenuBtn.addEventListener("click", function () {
             mobileMenu.classList.toggle("hidden");
-            mobileMenu.style.display = mobileMenu.style.display === "none" ? "flex" : "none";
         });
     });
 </script>
