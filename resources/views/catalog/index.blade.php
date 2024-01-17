26ceef33cd9cd721c79f1d37aadcd585cc5658ba
Tayen15 <tiantiantitik@gmail.com>
2024-01-17 09:49:02 +0700

    Make 2 card catalog column in mobile dimension

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -52,7 +52,7 @@
 
     @media (max-width: 425px) {
         .grid {
-            grid-template-columns: repeat(1, minmax(0, 1fr));
+            grid-template-columns: repeat(2, minmax(0, 1fr));
         }
     }
 </style>
