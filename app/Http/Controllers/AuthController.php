12e52ad9bd842aa5390c30bc06c9d14be72f7eb1
Tayen15 <tiantiantitik@gmail.com>
2024-01-19 14:21:12 +0700

    Add invalid session in func logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,9 +26,12 @@ class AuthController extends Controller
     }
 
     
-    public function logout()
+    public function logout(Request $request)
     {
         Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
 
         return redirect()->route('login');
     }
