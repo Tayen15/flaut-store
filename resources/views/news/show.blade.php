0fefd2448e0d5062e7c93023452e7ed68edb5bb8
Tayen15 <tiantiantitik@gmail.com>
2024-01-22 18:49:59 +0700

    Adding separator in author and publish date

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -24,8 +24,12 @@
                         <svg fill="#000000" width="20px" height="20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M15.71,12.71a6,6,0,1,0-7.42,0,10,10,0,0,0-6.22,8.18,1,1,0,0,0,2,.22,8,8,0,0,1,15.9,0,1,1,0,0,0,1,.89h.11a1,1,0,0,0,.88-1.1A10,10,0,0,0,15.71,12.71ZM12,12a4,4,0,1,1,4-4A4,4,0,0,1,12,12Z"/></svg>
                     </div>
                 
-                    <div class="mx-1 md:mx-1">
+                    <div class="ml-1 md:mx-1">
                         <p class="text-gray-600 mr-4 text-sm md:sm lg:text-sm xl:text-sm"> {{ $news->author }}</p>
+                    </div>
+    
+                    <div class="mr-2 md:mx-1">
+                        <p class="text-gray-800 font-semibold">/</p>
                     </div>
 
                     <div class="flex-shrink-0 ml-1 md:mx-3">
@@ -57,7 +61,15 @@
     </section>
 </div>
 
+<style>
+    ol {
+        list-style-type: decimal; 
+    }
 
+    li {
+        display: list-item; 
+    } 
+</style>
 
 
 
