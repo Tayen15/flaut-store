1613d6fab12b1be4bec0abc8eff61fb75042fbbf
Tayen15 <tiantiantitik@gmail.com>
2024-01-29 16:13:22 +0700

    Add checking users level and author

--- resources/views/dashboard/catalog/index.blade.php
+++ resources/views/dashboard/catalog/index.blade.php
@@ -39,7 +39,7 @@
                     <th class="p-2 md:p-4 border-b border-blue-gray-50">Name</th>
                     <th class="p-2 md:p-4 border-b border-blue-gray-50">Author</th>
                     <th class="p-2 md:p-4 border-b border-blue-gray-50">Category</th>
-                    <th class="p-2 md:p-4 border-b border-blue-gray-50">Price</th>
+                    <th class="p-2 md:p-6 border-b border-blue-gray-50">Price</th>
                     <th class="p-2 md:p-4 border-b border-blue-gray-50">Created</th>
                     <th class="p-2 md:p-4 border-b border-blue-gray-50">Last Update</th>
                     <th class="p-2 md:p-6  border-b border-blue-gray-50">Action</th>
@@ -57,32 +57,34 @@
                     <td class="p-2 md:p-4 border-b border-blue-gray-50">{{ $item->name }}</td>
                     <td class="p-2 md:p-4 border-b border-blue-gray-50">{{ $item->author }}</td>
                     <td class="p-2 md:p-4 border-b border-blue-gray-50 uppercase">{{ $item->category }}</td>
-                    <td class="p-2 md:p-4 border-b border-blue-gray-50">Rp {{ number_format($item->price, 0, ',', '.') }}</td>
+                    <td class="p-2 md:p-4 border-b border-blue-gray-50">Rp{{ number_format($item->price, 0, ',', '.') }}</td>
                     <td class="p-2 md:p-4 border-b border-blue-gray-50">{{ \Carbon\Carbon::parse($item->created_at)->format('l, d F Y') }}</td>
                     <td class="p-2 md:p-4 border-b border-blue-gray-50">{{ $item->updated_at->format('j F Y, H:i') }}</td>
                     <td class="p-2 md:p-6 border-b border-blue-gray-50">
                         <div class="flex space-x-2">
-                            <a href="{{ route('dashboard.catalog.edit', $item->id) }}">
-                                <button class="relative align-middle select-none font-medium text-center uppercase transition-all w-10 h-10 md:w-12 md:h-12 rounded-lg text-xs text-gray-900 hover:bg-gray-900/10 active:bg-gray-900/20" type="button">
+                            @if (auth()->user()->level > 1 || auth()->user()->name === $item->author)
+                                <a href="{{ route('dashboard.catalog.edit', $item->id) }}">
+                                    <button class="relative align-middle select-none font-medium text-center uppercase transition-all w-10 h-10 md:w-12 md:h-12 rounded-lg text-xs text-gray-900 hover:bg-gray-900/10 active:bg-gray-900/20" type="button">
+                                        <span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="h-4 w-4 md:h-5 md:w-5">
+                                                <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z"></path>
+                                            </svg>
+                                        </span>
+                                    </button>
+                                </a>
+                                <button onclick="confirmDelete('{{ $item->id }}')" class="relative align-middle select-none font-medium text-center uppercase transition-all w-10 h-10 md:w-12 md:h-12 rounded-lg text-xs text-gray-900 hover:bg-gray-900/10 active:bg-gray-900/20" type="button">
                                     <span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="h-4 w-4 md:h-5 md:w-5">
-                                            <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z"></path>
+                                        <svg fill="#000000" width="20px" height="20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M20,6H16V5a3,3,0,0,0-3-3H11A3,3,0,0,0,8,5V6H4A1,1,0,0,0,4,8H5V19a3,3,0,0,0,3,3h8a3,3,0,0,0,3-3V8h1a1,1,0,0,0,0-2ZM10,5a1,1,0,0,1,1-1h2a1,1,0,0,1,1,1V6H10Zm7,14a1,1,0,0,1-1,1H8a1,1,0,0,1-1-1V8H17Z"/>
                                         </svg>
                                     </span>
                                 </button>
-                            </a>
-                            <button onclick="confirmDelete('{{ $item->id }}')" class="relative align-middle select-none font-medium text-center uppercase transition-all w-10 h-10 md:w-12 md:h-12 rounded-lg text-xs text-gray-900 hover:bg-gray-900/10 active:bg-gray-900/20" type="button">
-                                <span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
-                                    <svg fill="#000000" width="20px" height="20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M20,6H16V5a3,3,0,0,0-3-3H11A3,3,0,0,0,8,5V6H4A1,1,0,0,0,4,8H5V19a3,3,0,0,0,3,3h8a3,3,0,0,0,3-3V8h1a1,1,0,0,0,0-2ZM10,5a1,1,0,0,1,1-1h2a1,1,0,0,1,1,1V6H10Zm7,14a1,1,0,0,1-1,1H8a1,1,0,0,1-1-1V8H17Z"/>
-                                    </svg>
-                                </span>
-                            </button>
-                            <form id="delete-form-{{ $item->id }}" action="{{ route('catalog.destroy', $item->id) }}" method="POST" style="display: none;">
-                                @csrf
-                                @method('DELETE')
-                                <input type="hidden" id="confirmDeleteItemId" value="">
-                            </form>
+                                <form id="delete-form-{{ $item->id }}" action="{{ route('catalog.destroy', $item->id) }}" method="POST" style="display: none;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <input type="hidden" id="confirmDeleteItemId" value="">
+                                </form>
+                            @endif
                         </div>
                     </td>
                 </tr>
