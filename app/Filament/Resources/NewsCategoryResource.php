a7746c55dd4ceede4cd418c3e46d9d32e16448b1
Tayen15 <tiantiantitik@gmail.com>
2025-01-26 21:43:41 +0700

    Add NewsCategory resource and management page for category news

---
+++ app/Filament/Resources/NewsCategoryResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\NewsCategoryResource\Pages;
+use App\Filament\Resources\NewsCategoryResource\RelationManagers;
+use App\Models\NewsCategory;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class NewsCategoryResource extends Resource
+{
+    protected static ?string $model = NewsCategory::class;
+
+    protected static ?string $navigationLabel = 'Manage Category News';
+
+    protected static ?string $slug = 'manage-category-news';
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationGroup = 'News';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageNewsCategories::route('/'),
+        ];
+    }
+}
